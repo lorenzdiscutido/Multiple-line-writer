d46bb0c550baea58a6d15bbb4cf723581cbf7834
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-04-26 00:16:46 +0800

    Added some comments to make it more interactive

--- Multiple line writer.py
+++ Multiple line writer.py
@@ -12,19 +12,26 @@ def write():
         my_life.write(Line + "\n")
 
         #Ask the user if they want to enter another line
-        choices = input("Do you want to enter another line?(y/n):")
+        choices= input("Do you want to enter another line?(y/n):")
+        
+        #make a comment
+        if choices.lower() == "y":
+            print("Nice line!")
+            
+        else:
+             print("Thank you")
             
         
         #Do a while loop to ask the user repeatedly
-        while choices == "y":
+        while choices.lower == "y":
             Line = ("Enter a line:")
             print("")
 
         #to write a new line on the file
-        my_life.write(Line + "\n")
+    my_life.write(Line + "\n")
 
         #ask again the user if they want to continue
-        choices = input("Do you want to enter another line?(y/n):")
+    choices = input("Do you want to enter another line?(y/n):")
     
     #close the file
     my_life.close()
