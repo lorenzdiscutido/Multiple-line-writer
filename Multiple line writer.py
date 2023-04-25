8d1750718fa61349c869ae913f713481f728498c
lorenzdiscutido <lorenzdiscutido@gmail.com>
2023-04-25 23:38:26 +0800

    To write what the user inputted to the file

--- Multiple line writer.py
+++ Multiple line writer.py
@@ -1,6 +1,14 @@
 #define the function write
 def write():
+
     #open mylife.txt and put the write function to it
     with open("mylife.txt", "w") as my_life:
+
         #put a variable to where the user will input their lines
         Line = ("Enter line:")
+        print("")
+
+        #to write a new line on the file
+        my_life.write(Line + "\n")
+        
+
